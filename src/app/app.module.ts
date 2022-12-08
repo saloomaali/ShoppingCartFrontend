85872989f1ec28c93d9080cbac997798d892ed7b
Salooma <saloomaali2000@gmail.com>
2022-12-08 08:14:29 +0530

    ui completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,61 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { SearchProductComponent } from './search-product/search-product.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { CarcdProductComponent } from './carcd-product/carcd-product.component';
 
+const myRouter:Routes = [
+  {
+    path : "",
+    component : AdminLoginComponent
+  },
+  {
+    path: "register",
+    component : UserSignupComponent
+  },
+  {
+    path: "add",
+    component : AddProductComponent
+  },
+  {
+    path:"view",
+    component : ViewProductComponent
+  },
+  {
+    path:"search",
+    component : SearchProductComponent
+  },
+  {
+    path : "userLogin",
+    component:UserLoginComponent
+  },
+  {
+    path: "cardProduct",
+    component : CarcdProductComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    UserSignupComponent,
+    AddProductComponent,
+    ViewProductComponent,
+    SearchProductComponent,
+    UserLoginComponent,
+    CarcdProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRouter),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
